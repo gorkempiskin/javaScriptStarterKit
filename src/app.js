d3712cd1ce9fdd9c606266b48d21b278e87c8af6
gorkempiskin <g.piskin@outlook.com>
2021-10-19 00:02:20 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,22 @@
+console.log("Hello World")
+
+
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+console.log(euroDun)
+//Array
+//camelCasing
+//PascalCasing
+let users = ["Konut","Emlak","Kamu Konut","özel","yurt","diğer"]
+
+console.log("<ul>")
+for (let index = 0; index < users.length; index++) {
+    console.log("<li>" +  users[index]+ " </li>")
+    
+}
+console.log("</ul>")
